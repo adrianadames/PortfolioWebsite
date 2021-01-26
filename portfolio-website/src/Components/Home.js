a3454468c31e9686538933789de674ba2a6b290e
adrian <adrianadames@gmail.com>
2021-01-26 16:22:42 -0500

    Updated link to newest version of resume.

--- portfolio-website/src/Components/Home.js
+++ portfolio-website/src/Components/Home.js
@@ -479,7 +479,7 @@ function Home() {
                                             </clipPath>
                                         </defs>
                                         <foreignObject width="100" height="100">
-                                            <a href = 'https://drive.google.com/file/d/16dBujTu1gr7l82idia3yWHwZ241vGO5T/view?usp=sharing'>
+                                            <a href = 'https://drive.google.com/file/d/1dfUn3FTn9d_eFysXAN33XDqy0VYQ4KK6/view?usp=sharing'>
                                                 <div style={{width:'100%', height:'100%'}}></div>
                                             </a>
                                         </foreignObject>
